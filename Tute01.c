944ab0abc43c5361dfa8f15e05c8b6ba31502425
Nipun Palliyaguru <99266866+IT21187278@users.noreply.github.com>
2022-03-03 19:41:40 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,16 @@
 #include <stdio.h>
 
 int main() {
+
+  int m1,m2;
+  float avg;
+
+  printf("Enter Marks 1 and Mark 2 : ");
+  scanf("%d%d",&m1,&m2);
+
+  avg=(float)(m1+m2)/2;
+  printf("Average of %d %d is : %.2f",m1,m2,avg);
+
   
   return 0;
 }
