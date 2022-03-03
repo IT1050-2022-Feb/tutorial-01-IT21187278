01ca2b15bc2868ab01d93184f3897c85b42c1f93
Nipun Palliyaguru <99266866+IT21187278@users.noreply.github.com>
2022-03-03 19:44:17 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -1,7 +1,7 @@
 /*Exercise 3 - Repetition
 
 Write a C program to calculate the sum of the numbers from 1 to n.
-Where n is a keyboard input.
+Where n is keyboard input.
 
 e.g.
 
@@ -13,7 +13,18 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+
+  int n,i,cal=0;
+
+  printf("Enter value :");
+  scanf("%d",&n);
+
   
+  for(i=1;i<=n;i++){
+    cal+=i;
+    
+      }
+  printf("Total value is :%d",cal);
   return 0;
 }
 
